1d76bd94dc3b848a73fba7cc356075deaae88deb
Tape-Dispenser <gramiakshaye@gmail.com>
2023-11-25 02:22:45 -0800

    big overhaul, cleaning is finally done, all lines are now stored internally as a class, with lexing and parsing being functions of those objects. parsing still doesn't work yet.

--- clean.py
+++ clean.py
@@ -1,4 +1,186 @@
-import os,re
+import os,re,base64
+import urcl_rules
+
+labelDict = {}
+macroDict = {}
+escapeCodeDict = {
+    "\\b":8,
+    "\\t":9,
+    "\\n":10,
+    "\\v":11,
+    "\\f":12,
+    "\\r":13,
+    "\\\\":92,
+    "\\'":39,
+    "\\\"":34,
+    "\\0":0,
+}
+
+
+class Opcode:
+    string = ""
+    type = ""
+
+    def __init__(self, str):
+        self.string = str
+    def __str__(self):
+        return self.string
+
+class Operand:
+    string = ""
+    type = ""
+
+    def __init__(self, str):
+        self.string = str
+    def __str__(self):
+        return self.string
+
+class Line:
+        
+    string = ""
+    tokens = []
+    originalLineNum = 0
+    realLineNum = 0
+    opcode = Opcode("")
+    operands = []
+    
+    
+    
+    def __init__(self, line, lineNum):
+        self.string = line
+        self.originalLineNum = lineNum
+    def __str__(self):
+        return self.string
+    
+    def lex(self):
+        opcode = ""
+        operands = []
+        if self.string.strip() != "":
+        
+            #print(self.string) # DEBUG LINE
+            pieces = self.string.split()
+            opcode = pieces[0].strip()      # pretty self-explanitory.
+    
+            # now do operand lexing
+            inList = False
+            operands = []
+            listRegex = r"\[.*?\]"
+            operandRegex = r"'.'|'..'|[.$%@#RrMm]?[A-Za-z0-9-_]+|~[+-]?[0-9]+"
+            operandsString = " ".join(pieces[1:])
+            result = re.fullmatch(listRegex, operandsString)
+            if type(result) == re.match:
+                operandsString = operandsString[1:-1]
+                inList = True                                
+            try:   
+                result = re.finditer(operandRegex, operandsString)
+                for x in result:
+                    operands.append(x.group(0))
+            except IndexError:
+                pass
+            if inList:
+                pass # this seemed like it might be useful some time    
+        self.tokens = operands.insert(0, opcode)
+
+    def parse(self):
+        opcode = self.tokens[0]
+        operands = self.tokens[1:]
+    
+        global labelDict,macroDict
+
+        outputOperands = []
+
+        for operand in operands:
+            if opcode[0] == '.':
+                linetype = 'label'
+                labelEntry = (opcode, self.originalLineNum)
+                labelDict.append(labelEntry)
+            elif opcode[0] == '@':
+                linetype = 'macro'
+            elif opcode.lower() == 'dw':
+                linetype = 'dw'
+            elif opcode.lower() == 'bits':
+                linetype = 'header'
+            elif opcode.lower() == 'minreg':
+                linetype = 'header'
+            elif opcode.lower() == 'minheap':
+                linetype = 'header'
+            elif opcode.lower() == 'run':
+                linetype = 'header'
+            elif opcode.lower() == 'minstack':
+                linetype = 'header'
+            else:
+                try:
+                    urcl_rules.urclOperationLengths[opcode.lower()]
+                    linetype = 'instruction'
+                except KeyError:
+                    print(f'Error on line {self.originalLineNum}: "{f"{opcode} {operand}"}": Unrecognized instruction "{opcode}"')
+                    return 'error'
+
+            if linetype == 'instruction':
+
+                #TODO : make this a pattern maching thingy
+            
+                regex = re.fullmatch(r'~[+-]?[0-9]+', operand)              # check for relative address
+                if type(regex) == re.match:
+                    outputOperands.append( (operand,'relative address') )
+                    continue
+            
+                regex = re.fullmatch(r'[rR$][-+]?[0-9]+', operand)               # check for register
+                if type(regex) == re.match:
+                    outputOperands.append( (operand,'register') )
+                    continue
+
+                regex = re.fullmatch(r'0[xX][0-9]+', operand)               # check for hex immediate    
+                if type(regex) == re.match:
+                    outputOperands.append( (operand,'hex immediate') )
+                    continue
+
+                regex = re.fullmatch(r'0[bB][01]+', operand)                # check for binary immediate
+                if type(regex) == re.match:
+                    outputOperands.append( (a,'binary immediate') )
+                    continue
+            
+                regex = re.fullmatch(r'[-+]?[0-9]+', operand)                    # check for decimal immediate
+                if type(regex) == re.match:
+                    outputOperands.append( (operand,'decimal immediate') )
+                    continue
+
+                regex = re.fullmatch(r'\'.\'', operand)                     # check for character immediate
+                if type(regex) == re.match:
+                    a = ord(operand[1])
+                    outputOperands.append( (a,'immediate') )
+                    continue
+
+                regex = re.fullmatch(r'\.[a-zA-Z0-9-_]', operand)           # check for labels, check that label is defined
+                if type(regex) == re.match:
+                    try:
+                        labelDict[operand]
+                        outputOperands.append(operand, '')
+                        continue
+                    except ValueError:
+                        print(f'Error: Undefined label "{operand}" referenced on line {self.originalLineNum}')
+                        return('error')
+
+                regex = re.fullmatch(r'[mM#][0-9]+', operand)               # check for heap pointer
+                if type(regex) == re.match:
+                    outputOperands.append( (operand,'heap pointer') )
+                    continue
+
+                regex = re.fullmatch(r'%[a-zA-Z0-9-_]', operand)            # check for port
+                if type(regex) == re.match:
+                    outputOperands.append( (operand, 'port') )
+                    continue
+
+                regex = re.fullmatch(r'@[a-zA-Z0-9-_]', operand)            # check for macro
+                if type(regex) == re.match:
+                    # TODO: implement something similar to labels for keeping track of and detecting undefined macros
+                    outputOperands.append( (operand, 'macro') )
+                    continue
+
+                print(f'Error: Unrecognized operand "{operand}" on line {self.originalLineNum}')
+                return('error')
+        return (linetype, outputOperands)
+        
 
 def info():
     print('Cleans URCL code')
@@ -66,30 +248,32 @@ def clean(file, options=[]):
         chars[key] = char
         codeString = codeString.replace(char, key, 1) # only replace 1 instance
 
+    codeString = re.sub(r'\/\*.*\*\/', "", codeString) # remove all internal "multiline" comments (example: ADD /* comment */ R1 R2 R3)
+    
     codeList = codeString.split("\n")  
 
     lines = []
     for i,line in enumerate(codeList):
-        
-        # i'm gonna come back and rewrite this in a way that's easy to 
         line = line.strip()                                             # remove leading and trailing whitespace
-        
-        # i'm gonna come back and rewrite this 
 
         if line != '':                                                  # check to make sure line isn't blank
-            line = line.split('//')                                     # anything after a comment is useless
-            if line[0].strip() != '':                                   # make sure there is code before the comment
-                line[0] = line[0].strip()                               
+            line = line.split('//')[0]                                     # anything after a comment is useless
+            if line.strip() != '':                                   # make sure there is code before the comment
+                line = line.strip()                               
                 lineNum = i+1
                 
-                lineList = line[0].split(" ")                           # line[0] is everything before the comment
+                lineList = line.split(" ")                           # line[0] is everything before the comment
                 outList = []
                 for piece in lineList:
                     piece = piece.strip()
                     if piece != '':
                         outList.append(piece)
                 outLine = " ".join(outList)
-                lines.append(f"{outLine} // {lineNum}") # add original line number to output for more readable errors (TODO: make this an option)
+                x = outLine.find("/*")
+                if x != -1:
+                    lines.append(outLine[:x] + f"// {lineNum}" + outLine[x:]) # add original line number to output for more readable errors
+                else:
+                    lines.append(outLine + f"// {lineNum}")
 
     codeString = "\n".join(lines)
     
@@ -104,69 +288,52 @@ def clean(file, options=[]):
             codeString = codeString.replace(text, "")
 
     # put chars and strings back
-    # it may be smart to convert these to decimal immediates while i'm at it
-    
+    # it may be smart to convert these to decimal immediates while i'm at it  
     
     for key in strings:
         immString = strings[key][1:-1]                                    # remove quote marks
         immList = []
-        for char in immString:
-            immList.append(str(ord(char)))                              
+        # find escape codes with regex
+        matches = re.finditer(r'(\\[a-wy-zA-WY-Z0-9])|(\\x[0-9A-Fa-f]{2})', immString) # TODO: i still need to implement the \xXX escape codes
+        for m in matches:
+            escCode = m.group(0)
+            try:
+                charInt = escapeCodeDict[escCode]
+                immString = immString.replace(escCode, chr(charInt))
+            except KeyError:
+                print(f"Warning: Invalid escape code \"{escCode}\", ignoring") # TODO: make this a cli flag on whether or not to ignore invalid escape codes
+
+        for char in immString: 
+            immList.append(str(ord(char)))     
+                                 
         listString = f"[{' '.join(immList)}]"                               # this should output an array of decimal immediates equivalent to the bytes of the given string
         codeString = codeString.replace(key, listString)
  
     for key in chars:
         immChar = chars[key][1:-1]                                    # remove quote marks
-        if (len(immChar) == 2)&(immChar[0] == "\\"):                    # replace escape codes
-            match immChar:
-                case "\\n":
-                    imm = "10" # ascii code for newline
-                case "\\r":
-                    imm = "13"
-                case "\\t":
-                    imm = "9"
-                case "\\b":
-                    imm = "8"
-                case "\\f":
-                    imm = "12"
-                case "\\v":
-                    imm = "11"
-                case "\\0":
-                    imm = "0"
-                case _:
-                    print(f"invalid escape code \"{immChar}\"")
-                    return ('error')
+        if (len(immChar) == 2)&(immChar[0] == '\\'):                    # replace escape codes
+            try:
+                imm = str(escapeCodeDict[immChar])
+            except KeyError:
+                print(f'Warning! ignored escape code \"{immChar}\"')
         else:
             imm = str(ord(immChar))
         codeString = codeString.replace(key, imm)
     
-    codeList = codeString.split("\n")  
+    codeList = codeString.split("\n")
 
     lines = []
+    
     for i,line in enumerate(codeList):
-        
-        # i'm gonna come back and rewrite this in a way that's easy to 
-        line = line.strip()                                             # remove leading and trailing whitespace
-        
-        # i'm gonna come back and rewrite this 
+        temp = line.split("//")
+        if temp[0].strip() != '':
+            lines.append(Line(temp[0].strip(),int(temp[1])))
+    for line in lines:
+        # lexing time :)
+        line.lex()
 
-        if line != '':                                                  # check to make sure line isn't blank
-            line = line.split('//')                                     # anything after a comment is useless
-            if line[0].strip() != '':                                   # make sure there is code before the comment
-                line[0] = line[0].strip()                               
-                lineNum = i+1
-                
-                lineList = line[0].split(" ")                           # line[0] is everything before the comment
-                outList = []
-                for piece in lineList:
-                    piece = piece.strip()
-                    if piece != '':
-                        outList.append(piece)
-                outLine = " ".join(outList)
-                lines.append(f"{outLine} // {lineNum}") # add original line number to output for more readable errors (TODO: make this an option)
-
-    codeString = "\n".join(lines)
+    codeString = "\n".join([str(line)for line in lines]) # some cool one line shit i learned a couple years ago
 
     with open(outFile, 'w') as f:                                       # output cleaned code to file
-        f.write(codeString)                                      
+        f.write(codeString)
     return outFile                                                      # return output file for other functions to reference
--- readme.md
+++ readme.md
@@ -12,6 +12,13 @@ Commands List:
   clean: cleans out whitespace, comments, and strings
     options:
       -h : display options list and info about command
-      -o <path> : delclare output file (default is ./out.urcl)
+      -o <path.urcl> : delclare output file (default is ./out.urcl)
       -n : preserve original line number (used for debugging)
       -a : only allow ascii characters in strings
+
+Supported Macros:
+  `@IMPORT <path.urcl> <name>`: imports all labels in <path.urcl> as `.<name>.label` (not implemented)
+  `@DEFINE <A> <B>`: defines <A> as a macro equivalent to <B>. If <B> contains spaces or newlines, it must be a string. (not implemented)
+  `@DEBUG`: pauses execution when code reaches this line. (not implemented)
+  `@DEBUG onwrite <A>`: pauses execution when memory address, register, or port <A> is written to. (not implemented)
+  `@DEBUG onread <A>`: pauses execution when memory address, register, or port <A> is read from. (not implemented)
